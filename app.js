48de5bed776d346929b4e713e6c9920669a5165a
Oliver. FP <86372903+Oliver-Fj@users.noreply.github.com>
2022-10-15 00:28:10 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// first button
+function rotate1() {
+    document.getElementById('hy1').classList.toggle('hy1');
+    document.getElementById('hy2').classList.toggle('hy2');
+    document.getElementById('hy3').classList.toggle('hy3');
+  }
+  
+  // second button
+  function rotate2() {
+    document.getElementById('hy4').classList.toggle('hy4');
+    document.getElementById('hy5').classList.toggle('hy5');
+    document.getElementById('hy6').classList.toggle('hy6');
+  }
+  
+  // third button
+  function rotate3() {
+    document.getElementById('hy7').classList.toggle('hy7');
+    document.getElementById('hy8').classList.toggle('hy8');
+    document.getElementById('hy9').classList.toggle('hy9');
+  }
+  
+  // fourth button
+  function rotate4() {
+    document.getElementById('hy10').classList.toggle('hy10');
+    document.getElementById('hy11').classList.toggle('hy11');
+    document.getElementById('hy12').classList.toggle('hy12');
+  }
